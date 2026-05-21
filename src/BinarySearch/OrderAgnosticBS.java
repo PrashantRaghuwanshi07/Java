b2d979f9483e14bb212d173637ba061b4db5cefe
PrashantRaghuwanshi07 <prashant.raghuwanshi2006@gmail.com>
2026-05-21 17:02:24 +0530

    Order Agnostic BS

---
+++ src/BinarySearch/OrderAgnosticBS.java
@@ -0,0 +1,45 @@
+package BinarySearch;
+
+public class OrderAgnosticBS {
+    static void main(String[] args) {
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {89, 45, 22, 18, 16, 15, 4, 3, 2, 0, -4, -12, -18};
+        int target = 22;
+        int ans = orderAgnosticBS(arr, target);
+        System.out.println(ans);
+    }
+    static int orderAgnosticBS(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        boolean isAsc;
+        if(arr[start] < arr[end]) {
+            isAsc = true;
+        }
+        else {
+            isAsc = false;
+        }
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == arr[mid]) {
+                return mid;
+            }
+            if(isAsc == true) {
+                if(target > arr[mid]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+            else {
+                if(target > arr[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
