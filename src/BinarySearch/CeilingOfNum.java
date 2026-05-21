8122069c73f05ace02d448f2d458c3d85a69c68e
PrashantRaghuwanshi07 <prashant.raghuwanshi2006@gmail.com>
2026-05-21 18:42:07 +0530

    Ceiling and Floor of no

---
+++ src/BinarySearch/CeilingOfNum.java
@@ -0,0 +1,53 @@
+package BinarySearch;
+// ceiling of no means smallest no which is equal or largest to that no
+
+public class CeilingOfNum {
+    static void main(String[] args) {
+//        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int[] arr = {18, 16, 14, 9, 5, 3, 2};
+        int target = 15;
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        boolean isAsc;
+        if(arr[start] < arr[end]) {
+            isAsc = true;
+        }
+        else {
+            isAsc = false;
+        }
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == arr[mid]) {
+                return arr[mid];
+            }
+            if(isAsc == true) {
+                if(target < arr[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            else{
+                if(target < arr[mid]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = end - 1;
+                }
+            }
+        }
+        // when while loop break, start = end + 1 then next big no is start
+        // (end target start)
+        if(isAsc == true) {
+            return arr[start];
+        }
+        else {
+            return arr[end];
+        }
+    }
+}
---
+++ src/BinarySearch/FloorOfNum.java
@@ -0,0 +1,54 @@
+package BinarySearch;
+//floor of no means greatest no which is equal or smallest to that no
+
+public class FloorOfNum {
+    static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+//        int[] arr = {18, 16, 14, 9, 5, 3, 2};
+        int target = 15;
+        int ans = floor(arr, target);
+        System.out.println(ans);
+    }
+    static int floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        boolean isAsc;
+        if(arr[start] < arr[end]) {
+            isAsc = true;
+        }
+        else {
+            isAsc = false;
+        }
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == arr[mid]) {
+                return arr[mid];
+            }
+            if(isAsc == true) {
+                if(target < arr[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            else{
+                if(target < arr[mid]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = end - 1;
+                }
+            }
+        }
+        // when while loop break, start = end + 1 then next big no is start
+        // (end target start)
+        if(isAsc == true) {
+            return arr[end];
+        }
+        else {
+            return arr[start];
+        }
+    }
+}
+
