cb7b56cdd1e77dd3292bc4578aa37a89d4c309d2
PrashantRaghuwanshi07 <prashant.raghuwanshi2006@gmail.com>
2026-05-27 18:25:10 +0530

    Selection sort code

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr) {
+         for(int i=0; i<arr.length; i++) {
+             int max = 0;
+             for(int j=0; j<arr.length-i; j++) {
+                 if (arr[j] > arr[max]) {
+                     max = j;
+                 }
+                 int temp = arr[max];
+                 arr[max] = arr[arr.length-i-1];
+                 arr[arr.length-i-1] = temp;
+             }
+         }
+    }
+}
